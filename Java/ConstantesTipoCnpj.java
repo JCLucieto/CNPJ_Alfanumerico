30d090235845a80395bcd6d26ec75f3e40661e03
Julio Lucieto <jlucieto@br.inter.net>
2025-02-10 11:56:33 -0300

    Add files via upload

---
+++ Java/ConstantesTipoCnpj.java
@@ -0,0 +1,11 @@
+package cnpj;
+
+public class ConstantesTipoCnpj {
+
+    public static final String ALFANUMERICO = "A";
+    public static final String NUMERICO = "N";
+    
+    private ConstantesTipoCnpj() {
+        throw new UnsupportedOperationException("Esta classe não pode ser instanciada.");
+    }    
+}
